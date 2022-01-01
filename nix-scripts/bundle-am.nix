2eccc8e61932a9c23733a9ddc5da5bc2b414fe6d
Vanilla <osu_Vanilla@126.com>
2022-01-01 17:15:21 +0800

    [!] nix-bundle_am.yml: Debugging...

--- nix-scripts/bundle-am.nix
+++ nix-scripts/bundle-am.nix
@@ -1,6 +1,7 @@
 { pkgs ? import <nixpkgs> { } }:
 let am = "arknights-mower"; in
+let cmd = ''nix-bundle "(pkgs.callPackage ./. { }).${am}" /bin/${am}''; in
 pkgs.mkShell {
   nativeBuildInputs = with pkgs; [ nix-bundle ];
-  shellHook = ''exec nix-bundle "(pkgs.callPackage ./. { }).${am}" /bin/${am}'';
+  shellHook = ''exec "${cmd} && cp $(${cmd}) ${am}"'';
 }
