275a3d2f91f47bfcde95ea1560e622fd072cd95a
Vanilla <osu_Vanilla@126.com>
2022-01-29 18:19:40 +0800

    [+] pkgs/apple-fonts/default.nix.

--- default.nix
+++ default.nix
@@ -6,5 +6,6 @@
   gnome-text-editor = pkgs.callPackage ./pkgs/gnome-text-editor { };
   tdesktop-bin = pkgs.callPackage ./pkgs/tdesktop-bin { };
 
+  apple-fonts = pkgs.callPackage ./pkgs/apple-fonts { inherit pkgs; };
   Win10_LTSC_2021_fonts = pkgs.callPackage ./pkgs/Win10_LTSC_2021_fonts { };
 }
