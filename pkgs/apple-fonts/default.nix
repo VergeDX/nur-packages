07cabb7037331edbdab25115cca9600b4a8b647f
Vanilla <osu_Vanilla@126.com>
2024-02-04 23:25:07 +0800

    update[pkgs/apple-fonts/default.nix]: Fix mismatched hash.

--- pkgs/apple-fonts/default.nix
+++ pkgs/apple-fonts/default.nix
@@ -3,26 +3,26 @@
   SF-Pro = (pkgs.callPackage ./common.nix {
     name = "SF-Pro";
     fontName = "SF Pro Fonts";
-    sha256 = "sha256-nkuHge3/Vy8lwYx9z+pvsQZfzrNIP4K0OutpPl4yXn0=";
+    sha256 = "sha256-Mu0pmx3OWiKBmMEYLNg+u2MxFERK07BQGe3WAhEec5Q=";
   });
   SF-Compact = (pkgs.callPackage ./common.nix {
     name = "SF-Compact";
     fontName = "SF Compact Fonts";
-    sha256 = "sha256-+Q4HInJBl3FLb29/x9utf7A55uh5r79eh/7hdQDdbSI=";
+    sha256 = "sha256-Mkf+GK4iuUhZdUdzMW0VUOmXcXcISejhMeZVm0uaRwY=";
   });
   SF-Mono = (pkgs.callPackage ./common.nix {
     name = "SF-Mono";
     fontName = "SF Mono Fonts";
-    sha256 = "sha256-pqkYgJZttKKHqTYobBUjud0fW79dS5tdzYJ23we9TW4=";
+    sha256 = "sha256-tZHV6g427zqYzrNf3wCwiCh5Vjo8PAai9uEvayYPsjM=";
   });
   SF-Arabic = (pkgs.callPackage ./common.nix {
     name = "SF-Arabic";
     fontName = "SF Arabic Fonts";
-    sha256 = "sha256-MKcrCBtrxjm1DUZuvf2NKzcAzaiBjw1KgoDbKphrYkc=";
+    sha256 = "sha256-Vvfl9ByKww55lE3RTIx4TOEfuyVkENCMbHLYFGLhp2o=";
   });
   NY = (pkgs.callPackage ./common.nix {
     name = "NY";
     fontName = "NY Fonts";
-    sha256 = "sha256-XOiWc4c7Yah+mM7axk8g1gY12vXamQF78Keqd3/0/cE=";
+    sha256 = "sha256-tn1QLCSjgo5q4PwE/we80pJavr3nHVgFWrZ8cp29qBk=";
   });
 }
